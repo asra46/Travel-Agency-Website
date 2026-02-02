f41af6103eb9a5dcad14ec11e0bddedcdadbddcd
asra46 <133696847+asra46@users.noreply.github.com>
2026-02-03 00:49:32 +0500

    Update Tours.jsx

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -17,7 +17,7 @@ const Tours = () => {
             <div className="card-body">
               <h5 className="card-title">{data.title}</h5>
               <p className="card-text">{data.description}</p>
-              <a href="#" className="btn btn-primary">
+              <a href="/" className="btn btn-primary">
                 Explore
               </a>
             </div>
